49b514aedfe9e2e3e3cd5578c9fe4226059b3412
bhaskargautam <bhaskar.iiita@gmail.com>
2018-01-12 06:43:35 +0100

    logging request

--- src/main/java/com/domain/resources/ApiResource.java
+++ src/main/java/com/domain/resources/ApiResource.java
@@ -1,5 +1,7 @@
 package com.domain.resources;
 
+import java.util.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -9,6 +11,9 @@ import javax.ws.rs.core.Response;
 
 @Path("apiserver")
 public class ApiResource {
+	
+	private static final Logger LOGGER = Logger.getLogger(ApiResource.class.getName() );
+	
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getIt() {
@@ -19,6 +24,7 @@ public class ApiResource {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public Response postIt(String req) {
+    		LOGGER.info(req);
         return Response.status(200).entity(req).build();
     }
 }               
